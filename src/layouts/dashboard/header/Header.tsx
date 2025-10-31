b7ff151069d82c050b6d9fd566da05af45b66376
khaytechdigitalz <60061496+khaytechdigitalz@users.noreply.github.com>
2025-10-31 11:09:07 +0100

    Updated

--- src/layouts/dashboard/header/Header.tsx
+++ src/layouts/dashboard/header/Header.tsx
@@ -16,8 +16,6 @@ import { useSettingsContext } from '../../../components/settings';
 import Searchbar from './Searchbar';
 import AccountPopover from './AccountPopover';
 import LanguagePopover from './LanguagePopover';
-import ContactsPopover from './ContactsPopover';
-import NotificationsPopover from './NotificationsPopover';
 
 // ----------------------------------------------------------------------
 
@@ -59,9 +57,7 @@ export default function Header({ onOpenNav }: Props) {
       >
         <LanguagePopover />
 
-        <NotificationsPopover />
-
-        <ContactsPopover />
+ 
 
         <AccountPopover />
       </Stack>
